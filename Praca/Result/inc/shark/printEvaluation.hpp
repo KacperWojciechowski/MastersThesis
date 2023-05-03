9cd0e9de918fa599af0ae9af88efe5b11b0ea0a3
Kacper Wojciechowski <k.woj.coding@gmail.com>
2023-05-03 18:42:32 +0000

    .

--- Praca/Result/inc/shark/printEvaluation.hpp
+++ Praca/Result/inc/shark/printEvaluation.hpp
@@ -33,27 +33,6 @@ inline void printSharkModelEvaluation(
     std::cout << "R^2: " << r_squared << std::endl;
 }
 
-template <typename LabelType, typename PredType,
-std::enable_if_t<std::is_same<remora::vector<double>, typename LabelType::LabelT>::value,
-    bool> = true> 
-inline void printSharkModelEvaluation(
-    const LabelType& labels, 
-    const PredType& predictions)
-{
-    using namespace shark;
-    using namespace shark::statistics;
-
-    // błąd średniokwadratowy
-    SquaredLoss<> loss;
-    auto mse = loss(labels, predictions);
-    std::cout << "MSE: " << mse << std::endl;
-    
-    // metryka R^2
-    auto var = Variance().statistics(labels);
-    auto r_squared = 1 - mse / var(0);
-    std::cout << "R^2: " << r_squared << std::endl;
-}
-
 template <typename LabelType, typename PredType>
 inline void printSharkModelEvaluation(
     const LabelType& labels, 
