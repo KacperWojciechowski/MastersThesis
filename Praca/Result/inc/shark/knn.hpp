350f5b81b35a016e0173368c609c1e0c3eb30281
Kacper Wojciechowski <k.woj.coding@gmail.com>
2023-05-03 18:59:27 +0000

    .

--- Praca/Result/inc/shark/knn.hpp
+++ Praca/Result/inc/shark/knn.hpp
@@ -17,19 +17,19 @@ inline void sharkKNN(const shark::ClassificationDataset& trainData,
 
     // utworzenie i konfiguracja drzewa oraz algorytmu
     KDTree<RealVector> tree(trainData.inputs());
-    TreeNearestNeighbors<RealVector,unsigned int> algorithm(
+    TreeNearestNeighbors<RealVector, unsigned int> algorithm(
         trainData, &tree);
 
     // konfiguracja modelu
     const unsigned int K = 2; // ilość sąsiadów dla algorytmu kNN
-    NearestNeighborModel<RealVector, unsigned int> KNN(&algorithm, K);
+    NearestNeighborModel<RealVector, RealVector> KNN(&algorithm, K);
 
     // ewaluacja modelu
     std::cout << "-----Shark KNN-----" << std::endl;
     std::cout << "Train data :" << std::endl;
     auto predictions = KNN(trainData.inputs());
     printSharkModelEvaluation(
-        static_cast<Data<unsigned int>>(trainData.labels()), predictions);
+        trainData.labels(), predictions);
 
     std::cout << "Test data:" << std::endl;
     predictions = KNN(testData.inputs());
