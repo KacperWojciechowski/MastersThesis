54be9a3fe985a2158f7e539ca0666892ac9bd817
Kacper Wojciechowski <k.woj.coding@gmail.com>
2023-05-03 16:12:06 +0000

    .

--- Praca/Result/test.cpp
+++ Praca/Result/test.cpp
@@ -1,9 +1,9 @@
-#include <inc/shark/sharkModels.hpp>
-//#include <inc/shogun/shogunModels.hpp>
+//#include <inc/shark/sharkModels.hpp>
+#include <inc/shogun/shogunModels.hpp>
 //#include <inc/dlib/dlibModels.hpp>
 
 int main()
 {
-    sharkModels();
-    //shogunModels();
+    //sharkModels();
+    shogunModels();
 }
