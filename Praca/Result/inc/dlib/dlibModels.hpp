d0616fcd8f51d92480ee0fdebfb6e676ca118007
root <root@DESKTOP-IR6AC0J.localdomain>
2023-06-01 00:22:33 +0200

    .

--- Praca/Result/inc/dlib/dlibModels.hpp
+++ Praca/Result/inc/dlib/dlibModels.hpp
@@ -1,9 +1,7 @@
 #pragma once
 
 #include <inc/dlib/linear.hpp>
-#include <inc/dlib/linear.hpp>
 #include <inc/dlib/svm.hpp>
-#include <inc/dlib/neural.hpp>
 
 #include <fstream>
 
@@ -18,6 +16,35 @@ inline void dlibModels()
     classFile >> classData;
     regFile >> regData;
 
-    std::cout << classData << std::endl << std::endl;
-    std::cout << regData << std::endl << std::endl;
+    auto classInputs = dlib::subm(classData, 0, 0, classData.nr(), classData.nc()-1);
+    auto classOutputs = dlib::subm(classData, 0, classData.nc()-1, classData.nr(), 1);
+
+    auto regInputs = dlib::subm(regData, 0, 0, regData.nr(), regData.nc()-1);
+    auto regOutputs = dlib::subm(regData, 0, regData.nc()-1, regData.nr(), 1);
+
+    using ClassSampleType = matrix<double, 11, 1>;
+    using RegSampleType = matrix<double, 5, 1>;
+
+    auto classTrainSamplesCount = static_cast<long>(classData.nr() * 0.8);
+    auto regTrainSamplesCount = static_cast<long>(regData.nr() * 0.8);
+
+    std::vector<classSampleType> classTestSamples;
+    std::vector<double> classTestLabels;
+    for (int row = 0; row < classTrainSamplesCount; row++)
+    {
+        classTestSamples.emplace_back(dlib::reshape_to_column_vector(
+	    dlib::subm_clipped(inputs, row, 0, 1, classData.nc())));
+	classTestLabels.emplace_back(classOutputs(row, 0));
+    }
+
+    std::vector<classSampleType> classTrainSamples;
+    std::vector<double> classTrainLabels;
+    for (int row = classTrainSamplesCount; row < classInputs.nr(); ++row)
+    {
+        classTrainSamples.emplace_back(dlib::reshape_to_column_vector(
+	    dlib::subm_clipped(classInputs, row, 0, 1, classData.nc())));
+	classTrainLabels.emplace_back(classOutputs(row, 0));
+    }
+
+
 }
