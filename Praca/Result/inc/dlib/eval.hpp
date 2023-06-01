04c6bf163c600051493ceb9f3ffc0be7205f29ab
root <root@DESKTOP-IR6AC0J.localdomain>
2023-06-01 19:21:42 +0200

    .

--- Praca/Result/inc/dlib/eval.hpp
+++ Praca/Result/inc/dlib/eval.hpp
@@ -20,11 +20,9 @@ inline void dlibEval(std::vector<double> predictions,
         // przygotowanie kontenerów na podzielone dane
         std::vector<double> correct;
         std::vector<double> incorrect;
-        correct.reserve(predictions.size());
-        incorrect.reserve(predictions.size());
         // przygotowanie wartości detektora
-        constexpr int positiveDetectionScore = 1;
-        constexpr int negativeDetectionScore = 0;
+        constexpr double positiveDetectionScore = 0.75;
+        constexpr double negativeDetectionScore = 0.25;
         // podział danych
         for (int i = 0; i < predictions.size() && i < labels.size(); ++i)
         {
@@ -39,7 +37,9 @@ inline void dlibEval(std::vector<double> predictions,
         double aucRoc = 0.0;
         for (int i = 0; i < roc.size() - 1; i++)
         {
+	    std::cout << "roc detection threshold=" << roc[i].detection_threshold << "; ";
             auto avg = (roc[i+1].true_positive_rate - roc[i].true_positive_rate)/2.0;
+	    std::cout << "roc[i]={" << roc[i].true_positive_rate << ", " << roc[i].false_positive_rate << "}\n";
             auto interval = roc[i+1].false_positive_rate - roc[i].false_positive_rate;
             aucRoc += avg * interval;
         }
